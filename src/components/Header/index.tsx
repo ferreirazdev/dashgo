3d9bdca171d2dbdc4823c5b98aa123c58fcc6f67
flavio <business.flavioferreira@gmail.com>
2021-09-28 16:55:12 -0300

    adding more responsive styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,8 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Flex, Icon, IconButton, useBreakpointValue } from '@chakra-ui/react';
+
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext';
+
+import { RiMenuLine } from 'react-icons/ri';
 
 import { Logo } from './Logo';
 import { SearchBox } from './SearchBox';
@@ -6,6 +10,8 @@ import { NotificationNav } from './NotificationNav';
 import { Profile } from './Profile';
 
 export function Header(): JSX.Element {
+
+  const { onOpen } = useSidebarDrawer();
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -23,6 +29,17 @@ export function Header(): JSX.Element {
       px="6"
       align="center"
     >
+        {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        />
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
