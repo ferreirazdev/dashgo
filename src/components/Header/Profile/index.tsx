7360b8a59e78badbf37b381fa34a6e843c4835a5
flavio <business.flavioferreira@gmail.com>
2021-09-28 16:35:30 -0300

    adding responsive styles

--- src/components/Header/Profile/index.tsx
+++ src/components/Header/Profile/index.tsx
@@ -1,18 +1,24 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile(): JSX.Element {
+import { IProfileProps } from './@interfaces';
+
+export function Profile({
+  showProfileData = true,
+}: IProfileProps): JSX.Element {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Flávio Ferreira</Text>
-        <Text color="gray.300" fontSize="small">
-          flavioferreiraz@protonmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Flávio Ferreira</Text>
+          <Text color="gray.300" fontSize="small">
+            flavioferreiraz@protonmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
-        name="Henrique Tavares"
+        name="Flávio Ferreira"
         src="https://github.com/ferreirazdev.png"
       />
     </Flex>
