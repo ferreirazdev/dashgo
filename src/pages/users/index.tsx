8bb6d3614cb7dc390cd00a44071f700646f5355d
flavio <business.flavioferreira@gmail.com>
2021-09-29 17:12:59 -0300

    using react query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -14,26 +15,43 @@ import {
   Tr,
   useBreakpointValue,
 } from '@chakra-ui/react';
+
 import { RiAddLine, RiDeleteBinLine, RiPencilLine } from 'react-icons/ri';
+
 import Link from 'next/link';
+import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
-import { useEffect } from 'react';
 
 export default function UsersList(): JSX.Element {
+  const { data, isLoading, error} = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    const users = data.users.map(user => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      }
+    })
+    return users
+  })
+
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }, []);
 
   return (
     <Box>
@@ -60,57 +78,71 @@ export default function UsersList(): JSX.Element {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de Cadastro</Th>}
-                <Th width="40" />
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Flávio Ferreira</Text>
-                    <Text fontSize="sm" color="gray.300">
-                      flavioferreiraz@protonmail.com
-                    </Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>07 de Abril, 2021</Td>}
-                {isWideVersion && (
-                  <Td>
-                    <Box
-                      display="flex"
-                      flexDir="row"
-                      alignItems="center"
-                      justifyContent="space-between"
-                    >
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        colorScheme="purple"
-                      >
-                        <Icon as={RiPencilLine} fontSize="20" />
-                      </Button>
-                      <Button as="a" size="sm" fontSize="sm" colorScheme="red">
-                        <Icon as={RiDeleteBinLine} fontSize="20" />
-                      </Button>
-                    </Box>
-                  </Td>
-                )}
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
+          { isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter os dados</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={['4', '4', '6']} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de Cadastro</Th>}
+                    <Th width="40" />
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {data.map(user => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px="6">
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        {isWideVersion && (
+                          <Td>
+                            <Box
+                              display="flex"
+                              flexDir="row"
+                              alignItems="center"
+                              justifyContent="space-between"
+                            >
+                              <Button
+                                as="a"
+                                size="sm"
+                                fontSize="sm"
+                                colorScheme="purple"
+                              >
+                                <Icon as={RiPencilLine} fontSize="20" />
+                              </Button>
+                              <Button as="a" size="sm" fontSize="sm" colorScheme="red">
+                                <Icon as={RiDeleteBinLine} fontSize="20" />
+                              </Button>
+                            </Box>
+                          </Td>
+                        )}
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
