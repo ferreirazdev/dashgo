d856ab0f773a030b981bf51e33d6ccd4f4079d27
flavio <business.flavioferreira@gmail.com>
2021-09-29 16:32:54 -0300

    mirage server with faker

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,11 +21,19 @@ import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
+import { useEffect } from 'react';
+
 export default function UsersList(): JSX.Element {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
 
   return (
     <Box>
