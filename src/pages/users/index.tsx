43613bc5e95e7110f0b5599ffad1dc205d3aa5e5
flavio <business.flavioferreira@gmail.com>
2021-09-30 14:08:23 -0300

    adding axios and creating hook

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,32 +19,16 @@ import {
 import { RiAddLine, RiDeleteBinLine, RiPencilLine } from 'react-icons/ri';
 
 import Link from 'next/link';
-import { useQuery } from 'react-query';
 
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
+import { useUsers } from '../../services/hooks/useUsers';
+
 
 export default function UsersList(): JSX.Element {
-  const { data, isLoading, error} = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users')
-    const data = await response.json()
-
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-    return users
-  })
+  const { data, isLoading, isFetching, refetch, error} = useUsers()
 
 
   const isWideVersion = useBreakpointValue({
@@ -63,6 +47,7 @@ export default function UsersList(): JSX.Element {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
 
             <Link href="/users/create" passHref>
